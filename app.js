115e77ab1bb075aae0c142eaeefd6d0164aeb21e
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-08-02 01:05:49 +0100

    api consumption and function creation

---
+++ app.js
@@ -0,0 +1,30 @@
+// Event Listeners
+const personalActivity = document
+  .getElementById("personalActivity")
+  .addEventListener("click", getPersonalActivity);
+
+const groupActivity = document
+  .getElementById("groupActivity")
+  .addEventListener("click", getGroupActivity);
+
+
+// FUNCTIONS
+function getPersonalActivity () {
+  const activityType = document.getElementById("activity-type").value;
+  const url = `https://www.boredapi.com/api/activity?type=${activityType}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function getGroupActivity () {
+const peopleCount = document.getElementById("people").value;
+  const url = `https://www.boredapi.com/api/activity?participants=${peopleCount}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}

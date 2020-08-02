fa2693b2f2f11748ff67a4ff500de12c843e299d
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-08-02 23:11:55 +0100

    code structuring

--- app.js
+++ app.js
@@ -7,44 +7,42 @@ const groupActivity = document
   .getElementById("groupActivity")
   .addEventListener("click", getGroupActivity);
 
-
 // FUNCTIONS
 
 //  Personal Activity Function
-function getPersonalActivity () {
+function getPersonalActivity() {
   const activityType = document.getElementById("activity-type").value;
   const url = `https://www.boredapi.com/api/activity?type=${activityType}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-
       // removes svg from page
       let imgRemove = document.getElementById("joy-svg");
-      imgRemove.remove();
+      // imgRemove.remove();
 
       let headerDiv = document.getElementById("header-div");
 
       // created the main card div
-      let response = document
-        .createElement("div")
-        .classList.add("card", "w-75", "p-5", "mx-auto");
+      let response = document.createElement("div");
+      response.classList.add("card", "w-75", "p-5", "mx-auto");
+
+      // Append main card Div to header Div
+      headerDiv.appendChild(response);
+
+      // Create sub card div
+      let subResponse = document.createElement("div");
+      subResponse.classList.add("card-body");
+
+      // Append sub card div to main card div
+      response.appendChild(subResponse);
+
       console.log(data);
     });
-  
-  // Add main card Div to header Div
-  headerDiv.appendChild(response);
-
-        // Create sub card div
-        let subResponse = document.createElement("div").classList.add("card-body");
-        console.log(data);
-    });
-  
-
 }
 
 // Group Activity Function
-function getGroupActivity () {
-const peopleCount = document.getElementById("people").value;
+function getGroupActivity() {
+  const peopleCount = document.getElementById("people").value;
   const url = `https://www.boredapi.com/api/activity?participants=${peopleCount}`;
   fetch(url)
     .then((response) => response.json())
